aea76d1c51fb50da61e083c6144577e36e06a2a9
weiwei-wch <54694643+weiwei-wch@users.noreply.github.com>
2019-09-06 00:08:01 +0800

    Update permute_group.R

--- R/permute_group.R
+++ R/permute_group.R
@@ -286,7 +286,7 @@ vertex_attr_perm_weighted <- function(measure, g, densities, xfm.type = c('1/w',
     s.score=lapply(g, function(x) t(sapply(x, s.score, A))),
     Lp.wt={g1 <- lapply(g, lapply, function(x) xfm.weights(x, xfm.type))
       Lpv_wt <- lapply(g1, function(x) t(sapply(x, function(y) Lpv_wt_gen(y))))
-      lapply(Lpv_wt, function(x) t(sapply(x, rowMeans, rm.na=TRUE)))},
+      lapply(Lpv_wt, function(x) t(sapply(x, rowMeans, na.rm=TRUE)))},
     E.local.wt={g1 <- lapply(g, lapply, function(x) xfm.weights(x, xfm.type))
       lapply(g1, function(x) t(sapply(x, efficiency, type='local', weights=NA, use.parallel=TRUE, A=A)))},
     E.nodal.wt={g1 <- lapply(g, lapply, function(x) xfm.weights(x, xfm.type))
